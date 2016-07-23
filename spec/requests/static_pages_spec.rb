a39f391146bb08a8101a362d892b6779201c0391
kbataykin <ylol2007@bk.ru>
2016-07-23 18:21:25 +0300

    bootstrap

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe "StaticPages", type: :request do
   describe "GET /static_pages" do
@@ -11,6 +12,10 @@ RSpec.describe "StaticPages", type: :request do
     it "works! (now write some real specs)" do
       get static_pages_help_path
       expect(response).to have_http_status('200')
+    end
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
   describe "GET /static_pages" do
